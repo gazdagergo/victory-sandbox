4376ef85d356315c7854e5d0a5287a3f3ecfc920
gazdagergo <info@gazdagergo.com>
2018-06-03 18:11:59 +0200

    line diagram with interpolation.
    https://formidable.com/open-source/victory/gallery/interpolation/

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import React from "react";
+import { render } from "react-dom";
+import { VictoryLine, VictoryChart, VictoryScatter } from "victory";
 
 const data = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 }
+  { x: 0, y: 0 },
+  { x: 1, y: 2 },
+  { x: 2, y: 1 },
+  { x: 3, y: 4 },
+  { x: 4, y: 3 },
+  { x: 5, y: 5 }
 ];
 
-const App = () => (
-  <VictoryChart
-    domainPadding={20}
-  >
-    <VictoryAxis
-      tickValues={[1, 2, 3, 4]}
-      tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-    />
-    <VictoryAxis
-      dependentAxis
-      tickFormat={(x) => (`$${x / 1000}k`)}
-    />
-    <VictoryBar
-      data={data}
-      x="quarter"
-      y="earnings"
-    />
-  </VictoryChart>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <VictoryChart height={290}>
+          <VictoryLine
+            interpolation="bundle"
+            data={data}
+            style={{ data: { stroke: "#c43a31" } }}
+          />
+          <VictoryScatter
+            data={data}
+            size={5}
+            style={{ data: { fill: "#c43a31" } }}
+          />
+        </VictoryChart>
+      </div>
+    );
+  }
+}
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
