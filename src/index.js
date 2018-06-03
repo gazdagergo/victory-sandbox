361dacb6a1586a2907edf4d157457a93f1663380
gazdagergo <info@gazdagergo.com>
2018-06-03 18:24:20 +0200

    brush and zoom
    delete bottom chart

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ class App extends React.Component {
   }
 
   handleZoom(domain) {
+    console.log(domain);
     this.setState({ selectedDomain: domain });
   }
 
@@ -38,48 +39,6 @@ class App extends React.Component {
             />
           }
         >
-          <VictoryLine
-            style={{
-              data: { stroke: "tomato" }
-            }}
-            data={[
-              { x: new Date(1982, 1, 1), y: 125 },
-              { x: new Date(1987, 1, 1), y: 257 },
-              { x: new Date(1993, 1, 1), y: 345 },
-              { x: new Date(1997, 1, 1), y: 515 },
-              { x: new Date(2001, 1, 1), y: 132 },
-              { x: new Date(2005, 1, 1), y: 305 },
-              { x: new Date(2011, 1, 1), y: 270 },
-              { x: new Date(2015, 1, 1), y: 470 }
-            ]}
-          />
-        </VictoryChart>
-
-        <VictoryChart
-          padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
-          width={600}
-          height={90}
-          scale={{ x: "time" }}
-          containerComponent={
-            <VictoryBrushContainer
-              responsive={false}
-              brushDimension="x"
-              brushDomain={this.state.selectedDomain}
-              onBrushDomainChange={this.handleBrush.bind(this)}
-            />
-          }
-        >
-          <VictoryAxis
-            tickValues={[
-              new Date(1985, 1, 1),
-              new Date(1990, 1, 1),
-              new Date(1995, 1, 1),
-              new Date(2000, 1, 1),
-              new Date(2005, 1, 1),
-              new Date(2010, 1, 1)
-            ]}
-            tickFormat={x => new Date(x).getFullYear()}
-          />
           <VictoryLine
             style={{
               data: { stroke: "tomato" }
