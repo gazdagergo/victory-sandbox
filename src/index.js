024964b28596f20839ecb75c5fa782b19295e238
gazdagergo <info@gazdagergo.com>
2018-06-03 19:32:37 +0200

    zoom and brush
    implement pannable, unzoomable chart with one week domain

--- src/index.js
+++ src/index.js
@@ -3,56 +3,66 @@ import { render } from "react-dom";
 import {
   VictoryZoomContainer,
   VictoryChart,
-  VictoryBrushContainer,
-  VictoryAxis,
-  VictoryLine
+  VictoryLine,
+  VictoryScatter
 } from "victory";
+
+const data = [
+  { x: new Date(2018, 6, 8), y: 5 },
+  { x: new Date(2018, 6, 9), y: 6 },
+  { x: new Date(2018, 6, 10), y: 2 },
+  { x: new Date(2018, 6, 11), y: 8 },
+  { x: new Date(2018, 6, 12), y: 4 },
+  { x: new Date(2018, 6, 13), y: 0 },
+  { x: new Date(2018, 6, 14), y: 9 },
+  { x: new Date(2018, 6, 15), y: 9 },
+  { x: new Date(2018, 6, 16), y: 4 },
+  { x: new Date(2018, 6, 17), y: 1 },
+  { x: new Date(2018, 6, 18), y: 12 },
+  { x: new Date(2018, 6, 19), y: 7 },
+  { x: new Date(2018, 6, 20), y: 3 },
+  { x: new Date(2018, 6, 21), y: 5 },
+  { x: new Date(2018, 6, 22), y: 4 }
+];
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      selectedDomain: { x: [new Date(2018, 6, 16), new Date(2018, 6, 22)] }
+    };
   }
 
   handleZoom(domain) {
-    console.log(domain);
     this.setState({ selectedDomain: domain });
-  }
-
-  handleBrush(domain) {
-    this.setState({ zoomDomain: domain });
   }
 
   render() {
     return (
       <div>
         <VictoryChart
-          width={600}
+          width={400}
           height={350}
           scale={{ x: "time" }}
+          domainPadding={{ y: [0, 20] }}
           containerComponent={
             <VictoryZoomContainer
+              allowZoom={false}
               responsive={false}
               zoomDimension="x"
-              zoomDomain={this.state.zoomDomain}
-              onZoomDomainChange={this.handleZoom.bind(this)}
+              zoomDomain={this.state.selectedDomain}
             />
           }
         >
           <VictoryLine
-            style={{
-              data: { stroke: "tomato" }
-            }}
-            data={[
-              { x: new Date(1982, 1, 1), y: 125 },
-              { x: new Date(1987, 1, 1), y: 257 },
-              { x: new Date(1993, 1, 1), y: 345 },
-              { x: new Date(1997, 1, 1), y: 515 },
-              { x: new Date(2001, 1, 1), y: 132 },
-              { x: new Date(2005, 1, 1), y: 305 },
-              { x: new Date(2011, 1, 1), y: 270 },
-              { x: new Date(2015, 1, 1), y: 470 }
-            ]}
+            interpolation="bundle"
+            style={{ data: { stroke: "#c43a31" } }}
+            data={data}
+          />
+          <VictoryScatter
+            data={data}
+            size={5}
+            style={{ data: { fill: "#c43a31" } }}
           />
         </VictoryChart>
       </div>
